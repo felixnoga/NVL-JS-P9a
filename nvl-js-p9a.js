f74e333779461d9435a7b8ca5d2e56f4d5fd2923
Felix Nogales <felixnoga@gmail.com>
2019-05-26 21:52:07 +0200

    Ejercicio arrays

---
+++ nvl-js-p9a.js
@@ -0,0 +1,13 @@
+var matriz = [[1,2], ["uno", "dos"], [1, "dos"]];
+
+var longitud_array = matriz.length;
+
+//Tercer elemento del array
+var tercer_elemento = matriz[2];
+
+//Segundo elemento del tercer elemento
+var segundo_de_tercer = matriz[2][1];
+
+console.log(longitud_array);
+console.log(tercer_elemento);
+console.log(segundo_de_tercer);
